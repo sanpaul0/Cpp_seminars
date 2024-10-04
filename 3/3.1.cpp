f1dc40eb1ba1d9961e2b83c4c85b162aadc6b1f2
Ivan Pylev <145129762+sanpaul0@users.noreply.github.com>
2024-10-04 20:57:10 +0300

    f

---
+++ 3/3.1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+void fill_array(int *arr, int size){
+    for (int i =0; i<size; i++){
+        arr[i] = i;
+    }
+}
+
+void print_array(int *arr, int size){
+    for (int i=0; i<size; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+int main(){
+    int arrsize, arrtype;
+    cin >> arrtype; // 1 если динамческий, если статический - любое другое 
+    cin >> arrsize;
+
+    #define SIZE arrsize
+    #define DINAMYC_MEMORY arrtype
+
+    #if (DINAMYC_MEMORY == 1)
+    arr = new int[SIZE]
+    fill_array(&arr, SIZE);
+    print_array(&arr, SIZE);
+    delete[] arr;
+
+    #else 
+    int arr[SIZE];
+    fill_array(arr, SIZE);
+    print_array(arr, SIZE);
+    #endif
+}
